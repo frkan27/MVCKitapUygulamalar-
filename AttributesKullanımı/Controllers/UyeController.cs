9c903eef21adb6727b1e3aaba0c1a060fc0b25b5
Furkan Kılıç <furkankilicytu@gmail.com>
2019-01-31 22:11:28 +0300

    uye ekranı

---
+++ AttributesKullanımı/Controllers/UyeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AttributesKullanımı.Controllers
+{
+    public class UyeController : Controller
+    {
+      public ActionResult Giris()
+        {
+            return View();
+
+        }
+    }
+}
---
+++ AttributesKullanımı/Models/UyeGiris.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AttributesKullanımı.Models
+{
+    public class UyeGiris
+    {
+        [DisplayName("Kullanıcı Adı")]
+        public string KullaniciAdi { get; set; }
+        [DisplayName("Şifre")]
+        [DataType(DataType.Password)]
+        public string Sifre { get; set; }
+        [DisplayName("Şifre Tekrarı")]
+        [DataType(DataType.Password)]
+        public string Sifre2 { get; set; }
+
+    }
+}

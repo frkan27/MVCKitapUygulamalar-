b041007d959fef69892be6b323439946a3405080
Furkan Kılıç <furkankilicytu@gmail.com>
2019-02-09 22:36:26 +0300

    Tema giydirildi.

---
+++ Tema/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tema.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            
+
+            return View();
+        }
+
+        public ActionResult Product()
+        {
+           
+
+            return View();
+        }
+
+        public ActionResult Store()
+        {
+            return View();
+        }
+    }
+}

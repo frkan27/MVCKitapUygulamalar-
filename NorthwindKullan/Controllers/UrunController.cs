f1ef1f284d0fc0d600a0d7cb1edc23080a426b5f
Furkan Kılıç <furkankilicytu@gmail.com>
2019-02-10 18:02:51 +0300

    ürün ekleme sıkıntılı

---
+++ NorthwindKullan/Controllers/UrunController.cs
@@ -0,0 +1,46 @@
+﻿using NorthwindKullan.Models.EntityModel;
+using NorthwindKullan.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NorthwindKullan.Controllers
+{
+    public class UrunController : Controller
+    {
+        NorthwindEntities db = new NorthwindEntities();
+        // GET: Urun
+        public ActionResult Index()
+        {
+            var model = db.Products.ToList();
+            return View(model);
+        }
+
+        public ActionResult Ekle()
+        {
+            var model = new UrunViewModel()
+            {
+                Categories=db.Categories.OrderBy(x=>x.CategoryName).ToList()
+            };
+
+            return View("UrunForm",model);
+        }
+        
+        public ActionResult Kaydet(Product urun)
+        {
+            if(urun.CategoryID==0)
+            {
+                db.Products.Add(urun);
+            }
+            else //guncelleme
+            {
+               
+            }
+            db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ NorthwindKullan/ViewModels/UrunViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NorthwindKullan.Models.EntityModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NorthwindKullan.ViewModels
+{
+    public class UrunViewModel//viewmodel yapmamnın tek nedeni liste seklinde katgori istemem.
+    {
+        public IEnumerable<Category> Categories { get; set; }
+        public Product Product { get; set; }
+    }
+}

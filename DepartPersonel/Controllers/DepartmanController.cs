4cc03d374d92b18d18bfac8abfdcd6ed3234d2a4
Furkan Kılıç <furkankilicytu@gmail.com>
2019-02-10 12:38:48 +0300

    silinid

--- DepartPersonel/Controllers/DepartmanController.cs
+++ DepartPersonel/Controllers/DepartmanController.cs
@@ -10,32 +10,46 @@ namespace DepartPersonel.Controllers
     public class DepartmanController : Controller
     {
         MyContext db = new MyContext();
-        public static List<Departman> departman { get; set; }
-        
+       
+      // public static List<Departman> dep { get; set; }
         // GET: Departman
         public ActionResult Index()
         {
-            var model = new List<Departman>()
-            {
-                new Departman()
-                {
-                    Ad="BilgiİŞlem",
-                    Id=1
-                },
-                new Departman()
-                {
-                    Ad="Muhasebe",
-                    Id=2
-                },
-                 new Departman()
-                {
-                    Ad="Satış",
-                    Id=3
-                },
+            //var model = new List<Departman>()
+            //{
+            //    new Departman()
+            //    {
+            //        Ad="Bilgiİşlem",
+            //        Id=1
+            //    },
+            //    new Departman()
+            //    {
+            //        Ad="Muhasebe",
+            //        Id=2
+            //    },
+            //     new Departman()
+            //    {
+            //        Ad="Satış",
+            //        Id=3
+            //    },
 
 
-            };
-            return View(model);
+            //};
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Yeni()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Yeni(Departman model)
+        {
+            
+            db.Departmanlar.Add(model);
+            db.SaveChanges();
+            return RedirectToAction("Index","Departman");
         }
     }
 }

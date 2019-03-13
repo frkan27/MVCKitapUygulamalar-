7657805b4805e3111c39f4cebf841cc11ce877e2
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-13 23:16:41 +0300

    ilkapiprojme

---
+++ İlkAPIProjem/Controllers/SehirController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace İlkAPIProjem.Controllers
+{
+    public class SehirController : ApiController
+    {
+        private string[] sehirler = new string[] { "Antalya", "Bursa", "İstanbul" };
+        public string[] Get()
+        {
+            return sehirler;
+        }
+        public string Get(int id)
+        {
+            return sehirler[id];//http://localhost:38312/api/sehir/1 yazınca 1. ındex ieskisehiri getiriryor
+        }
+
+    }//incele-network ten ne istek attığımıza bakabiliriz...
+}

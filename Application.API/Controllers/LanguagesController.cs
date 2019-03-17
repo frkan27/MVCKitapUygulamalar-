262c53f2846553866337317c687be56cec4faacd
Furkan Kılıç <furkankilicytu@gmail.com>
2019-03-17 16:48:45 +0300

    controller ok

---
+++ Application.API/Controllers/LanguagesController.cs
@@ -0,0 +1,36 @@
+﻿using Programing.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Application.API.Controllers
+{
+    public class LanguagesController : ApiController
+    {
+        LanguageDal LanguageDal = new LanguageDal();
+        public IEnumerable<language> Get()
+        {
+            return LanguageDal.GetAllLanguage();
+        }
+        public language Get(int id)
+        {
+            return LanguageDal.GetLanguageId(id);
+        }
+        public language Post(language language )
+        {
+            return LanguageDal.CreateLanguage(language);
+        }
+
+        public language Put(int id,language language)
+        {
+            return LanguageDal.UpdateLanguage(id, language);
+        }
+        public void Delete(int id)
+        {
+            LanguageDal.DeleteLanguage(id);
+        }
+    }
+}
